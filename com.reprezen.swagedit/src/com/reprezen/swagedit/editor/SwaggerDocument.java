4a149f99340e6e98089dad9aab822ae44f1d718d
ghillairet <g.hillairet@gmail.com>
2015-10-09 11:29:55 +0200

    Add tests for validator.

--- com.reprezen.swagedit/src/com/reprezen/swagedit/editor/SwaggerDocument.java
+++ com.reprezen.swagedit/src/com/reprezen/swagedit/editor/SwaggerDocument.java
@@ -4,20 +4,14 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.jface.text.Document;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.events.Event;
 import org.yaml.snakeyaml.events.Event.ID;
-import org.yaml.snakeyaml.nodes.MappingNode;
-import org.yaml.snakeyaml.nodes.Node;
-import org.yaml.snakeyaml.nodes.NodeTuple;
-import org.yaml.snakeyaml.nodes.ScalarNode;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.github.fge.jsonschema.core.report.ProcessingMessage;
 
 public class SwaggerDocument extends Document {
 
@@ -59,91 +53,6 @@ public class SwaggerDocument extends Document {
 		}
 
 		return events;
-	}
-
-	/*
-	 * Returns the line for which an error message has been produced.
-	 *
-	 *  The error message is a JSON object that contains the path to
-	 *  the invalid node. The path is accessible via instance.pointer.
-	 *  The path is in the forms:
-	 *  - /{id}
-	 *  - /{id}/~{nb}
-	 *  - /{id}/~{id2}
-	 *
-	 *  The line number is computed by after parsing the yaml content with
-	 *  the yaml parser. This latter returns a tree of Node, each corresponding
-	 *  to a yaml construct and including a position.
-	 *
-	 *  The Node matching the path is found by the methods findNode().
-	 */
-	public int getLine(ProcessingMessage message) {
-		final Reader reader = new StringReader(get());
-		final Node node = yaml.compose(reader);
-		final JsonNode m = message.asJson();
-		String path = m.get("instance").get("pointer").asText();
-		path = path.substring(1, path.length());
-		String[] strings = path.split("/");
-
-		if (node instanceof MappingNode) {
-			MappingNode mn = (MappingNode) node;
-
-			Node findNode = findNode(mn, Arrays.asList(strings));
-			if (findNode != null) {
-				return findNode.getStartMark().getLine() + 1;
-			}
-		}
-		return 1;
-	}
-
-	/*
-	 * Returns the yaml node that matches the given path.
-	 *
-	 * The path is given as a list of String. The Node matching the
-	 * path is found by traversing the children of the node pass as
-	 * first parameter.
-	 *
-	 */
-	private Node findNode(MappingNode root, List<String> paths) {
-		if (paths.isEmpty())
-			return root;
-
-		String path = paths.get(0);
-		if (path.startsWith("/")) {
-			path = path.substring(1, path.length());
-		}
-
-		final List<String> next = paths.subList(1, paths.size());
-
-		if (path.startsWith("~")) {
-			path = path.substring(1, path.length());
-			// path positions start at 1
-			int pos = Integer.valueOf(path) - 1;
-			if (pos >= 0 && pos < root.getValue().size()) {
-				NodeTuple nodeTuple = root.getValue().get(pos);
-				if (nodeTuple != null) {
-					return findNode(nodeTuple, next);
-				}
-			}
-		} else {
-			for (NodeTuple child: root.getValue()) {
-				if (child.getKeyNode() instanceof ScalarNode) {
-					ScalarNode scalar = (ScalarNode) child.getKeyNode();
-
-					if (scalar.getValue().equals(path)) {
-						return findNode(child, next);
-					}
-				}
-			}
-		}
-		return root;
-	}
-
-	private Node findNode(NodeTuple child, List<String> paths) {
-		if (child.getValueNode() instanceof MappingNode) {
-			return findNode((MappingNode) child.getValueNode(), paths);
-		}
-		return null;
 	}
 
 }
--- com.reprezen.swagedit/src/com/reprezen/swagedit/validation/SwaggerError.java
+++ com.reprezen.swagedit/src/com/reprezen/swagedit/validation/SwaggerError.java
@@ -1,9 +1,5 @@
 package com.reprezen.swagedit.validation;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
@@ -12,41 +8,34 @@ import org.eclipse.jface.text.IDocument;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.github.fge.jsonschema.core.report.LogLevel;
 import com.github.fge.jsonschema.core.report.ProcessingMessage;
-import com.github.fge.jsonschema.core.report.ProcessingReport;
-import com.reprezen.swagedit.editor.SwaggerDocument;
 
 public class SwaggerError {
 
 	private final int level;
 	private final String message;
-	private final Object original;
+	private final int line;
 
 	public SwaggerError(int level, String message) {
-		this(level, message, null);
+		this(level, message, 1);
 	}
 
-	public SwaggerError(int level, String message, Object original) {
+	public SwaggerError(int level, String message, int line) {
 		this.level = level;
 		this.message = message;
-		this.original = original;
+		this.line = line;
 	}
 
 	public static SwaggerError create(JsonMappingException exception) {
-		return new SwaggerError(IMarker.SEVERITY_ERROR, exception.getMessage(), exception);
-	}
-
-	public static SwaggerError create(ProcessingMessage message) {
-		return new SwaggerError(getLevel(message), message.getMessage(), message);
-	}
-
-	public static List<SwaggerError> create(ProcessingReport report) {
-		final List<SwaggerError> errors = new ArrayList<>();
-		if (report != null) {
-			for (Iterator<ProcessingMessage> it = report.iterator(); it.hasNext();) {
-				errors.add(create(it.next()));
-			}
+		int line = 1;
+		if (exception.getLocation() != null) {
+			line = exception.getLocation().getLineNr();
 		}
-		return errors;
+
+		return new SwaggerError(IMarker.SEVERITY_ERROR, exception.getMessage(), line);
+	}
+
+	public static SwaggerError create(ProcessingMessage message, int line) {
+		return new SwaggerError(getLevel(message), message.getMessage(), line);
 	}
 
 	protected static int getLevel(ProcessingMessage message) {
@@ -69,26 +58,28 @@ public class SwaggerError {
 
 	public IMarker addMarker(IFile target, IDocument document) throws CoreException {
 		final IMarker marker = target.createMarker(IMarker.PROBLEM);
-		marker.setAttribute(IMarker.SEVERITY, level);
-		marker.setAttribute(IMarker.MESSAGE, message);
-		marker.setAttribute(IMarker.LINE_NUMBER, 1);
-
-		if (original instanceof JsonMappingException) {
-			JsonMappingException exception = (JsonMappingException) original;
-
-			if (exception.getLocation() != null) {
-				marker.setAttribute(IMarker.LINE_NUMBER, exception.getLocation().getLineNr());
-			}
-		} else if (original instanceof ProcessingMessage) {
-			ProcessingMessage message = (ProcessingMessage) original;
-
-			if (document instanceof SwaggerDocument) {
-				SwaggerDocument sd = (SwaggerDocument) document;
-				marker.setAttribute(IMarker.LINE_NUMBER, sd.getLine(message));
-			}
-		}
+		marker.setAttribute(IMarker.SEVERITY, getLevel());
+		marker.setAttribute(IMarker.MESSAGE, getMessage());
+		marker.setAttribute(IMarker.LINE_NUMBER, getLine());
 
 		return marker;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public int getLine() {
+		return line;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public String toString() {
+		return "{ (level=" + getLevel() + ") " + getMessage() + " at line " + getLine() + " }";
 	}
 
 }
