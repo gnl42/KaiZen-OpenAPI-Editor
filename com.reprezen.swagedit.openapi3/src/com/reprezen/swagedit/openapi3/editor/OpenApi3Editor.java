26c79ad8b003fa0968699b153b69a822df7ab3bf
Tatiana Fesenko <tatiana.fesenko@gmail.com>
2017-04-26 12:00:28 -0400

    ZEN-3436 - wizard to create a new OpenAPI v3 spec

--- com.reprezen.swagedit.openapi3/src/com/reprezen/swagedit/openapi3/editor/OpenApi3Editor.java
+++ com.reprezen.swagedit.openapi3/src/com/reprezen/swagedit/openapi3/editor/OpenApi3Editor.java
@@ -2,6 +2,7 @@ package com.reprezen.swagedit.openapi3.editor;
 
 import org.dadacoalition.yedit.editor.YEdit;
 
-public class OpenApi3Editor extends YEdit{
+public class OpenApi3Editor extends YEdit {
+    public static final String ID = "com.reprezen.swagedit.openapi3.editor";
 
 }
