962e0d62b988a4f6d5f00fef880cb8a665c9f167
Tatiana Fesenko <tatiana.fesenko@gmail.com>
2017-05-03 10:19:47 -0400

    ZEN-3439 - basic hyperlink navigation for OpenAPI v3

--- com.reprezen.swagedit.openapi3/src/com/reprezen/swagedit/openapi3/editor/OpenApi3Editor.java
+++ com.reprezen.swagedit.openapi3/src/com/reprezen/swagedit/openapi3/editor/OpenApi3Editor.java
@@ -2,12 +2,16 @@ package com.reprezen.swagedit.openapi3.editor;
 
 import org.dadacoalition.yedit.editor.YEditSourceViewerConfiguration;
 import org.eclipse.jface.text.contentassist.ContentAssistant;
+import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
+import org.eclipse.jface.text.hyperlink.URLHyperlinkDetector;
+import org.eclipse.jface.text.source.ISourceViewer;
 
 import com.reprezen.swagedit.common.editor.JsonEditor;
 import com.reprezen.swagedit.common.editor.JsonSourceViewerConfiguration;
 import com.reprezen.swagedit.core.assist.JsonContentAssistProcessor;
 import com.reprezen.swagedit.openapi3.Activator;
 import com.reprezen.swagedit.openapi3.assist.OpenApi3ContentAssistProcessor;
+import com.reprezen.swagedit.openapi3.hyperlinks.OpenApi3ReferenceHyperlinkDetector;
 
 public class OpenApi3Editor extends JsonEditor {
 
@@ -33,6 +37,11 @@ public class OpenApi3Editor extends JsonEditor {
 		@Override
 		protected JsonContentAssistProcessor createContentAssistProcessor(ContentAssistant ca) {
 			return new OpenApi3ContentAssistProcessor(ca);
+		}
+
+		@Override
+		public IHyperlinkDetector[] getHyperlinkDetectors(ISourceViewer sourceViewer) {
+			return new IHyperlinkDetector[] { new URLHyperlinkDetector(), new OpenApi3ReferenceHyperlinkDetector() };
 		}
 
 	}
---
+++ com.reprezen.swagedit.openapi3/src/com/reprezen/swagedit/openapi3/hyperlinks/OpenApi3ReferenceHyperlinkDetector.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2016 ModelSolv, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    ModelSolv, Inc. - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package com.reprezen.swagedit.openapi3.hyperlinks;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.text.IRegion;
+
+import com.fasterxml.jackson.core.JsonPointer;
+import com.reprezen.swagedit.common.editor.JsonDocument;
+import com.reprezen.swagedit.core.hyperlinks.JsonFileHyperlink;
+import com.reprezen.swagedit.core.hyperlinks.JsonReferenceHyperlinkDetector;
+import com.reprezen.swagedit.openapi3.editor.OpenApi3Document;
+
+public class OpenApi3ReferenceHyperlinkDetector extends JsonReferenceHyperlinkDetector {
+
+	@Override
+	protected JsonFileHyperlink createFileHyperlink(IRegion linkRegion, String label, IFile file, JsonPointer pointer) {
+		return new SwaggerFileHyperlink(linkRegion, label, file, pointer);
+	}
+	
+	public static class SwaggerFileHyperlink extends JsonFileHyperlink {
+
+		public SwaggerFileHyperlink(IRegion linkRegion, String label, IFile file, JsonPointer pointer) {
+			super(linkRegion, label, file, pointer);
+		}
+
+		@Override
+		protected JsonDocument createDocument() {
+			return new OpenApi3Document();
+		}
+		
+	}
+
+}
