07fc0b5ab23b3af51139580fee526bd814c20b05
Tatiana Fesenko <tatiana.fesenko@gmail.com>
2017-05-08 15:40:38 -0400

    ZEN-3514 KZOE: Change SwagEdit to KaiZen OpenAPI Editor in UI
    Address comments to PR #279: 
    File > New Dialog
    
    Category should be "KaiZen OpenAPI Editor" (no hyphens)
    * Items should be named consistently.
    * First option should be "Swagger v2 Spec" (Should not include the word
    "New" in this context, and should include "v2" for clarity +
    consistency.)
    * Second option should be "OpenAPI v3 Spec" (with upper case 'S' in
    'Spec').
    
    
    Preferences Dialog:
    * Category can be just "KaiZen"
    * Description should be "KaiZen OpenAPI Editor Preferences" (without
    hyphens).
    * Sub-category should be "OpenAPI v3" (No space between "Open" and
    "API")

--- com.reprezen.swagedit.core/src/com/reprezen/swagedit/core/preferences/KaizenPreferencePage.java
+++ com.reprezen.swagedit.core/src/com/reprezen/swagedit/core/preferences/KaizenPreferencePage.java
@@ -18,7 +18,7 @@ public class KaizenPreferencePage extends FieldEditorPreferencePage implements I
 
     public KaizenPreferencePage() {
         super(GRID);
-        setDescription("KaiZen-OpenAPI-Editor Preferences");
+        setDescription("KaiZen OpenAPI Editor Preferences");
     }
 
     @Override
