a0278c4fc8f35ffbca10fcabf17c18726bf79f53
Tatiana Fesenko <tatiana.fesenko@gmail.com>
2017-05-24 16:51:54 -0400

    Content Type for v3 should allow unquoted values, e.g. 'openapi: 3.0.0'

--- com.reprezen.swagedit.openapi3/src/com/reprezen/swagedit/openapi3/editor/OpenApi3ContentDescriber.java
+++ com.reprezen.swagedit.openapi3/src/com/reprezen/swagedit/openapi3/editor/OpenApi3ContentDescriber.java
@@ -17,7 +17,7 @@ import com.reprezen.swagedit.core.editor.TextContentDescriber;
 public class OpenApi3ContentDescriber extends TextContentDescriber {
 	
 	public static final String CONTENT_TYPE_ID = "com.reprezen.swagedit.contenttype.openapi3.yaml";
-	private final Pattern openApiV3Regex = Pattern.compile(".*openapi:\\s+[\"']3\\.0\\..+", Pattern.DOTALL);
+	private final Pattern openApiV3Regex = Pattern.compile(".*openapi:\\s+[\"']?3\\.0\\..+", Pattern.DOTALL);
 
     @Override
     protected boolean isSupported(String content) {
